bae1f75227ddecd2c2cd3cd4cf2d693b00f0374c
annunziatadr99 <anudar99@hotmail.it>
2024-11-11 00:37:04 +0100

    continuare dal n 9

--- JS/D4.js
+++ JS/D4.js
@@ -1,6 +1,12 @@
 /* ESERCIZIO 1
  Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato.
 */
+const area = function(l1,l2) {
+  return l1 * l2;
+  
+}
+const rettangolo = area(3,5)
+console.log (rettangolo)
 
 /* SCRIVI QUI LA TUA RISPOSTA */
 
@@ -11,43 +17,49 @@
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-function crazySum(num1, num2) {
+const crazySum = function (num1, num2) {
   if (num1 === num2) {
     return (num1 + num2) * 3;
   } else {
     return num1 + num2;
   }
-  console.log(crazySum(6, 5));
-  console.log(crazySum(6, 6));
+
 }
+const molt = crazySum (7,7)
+console.log (molt)
 /* ESERCIZIO 3
  Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
  Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-function crazyDiff(num3) {
+const crazyDiff= function (num3) {
   if (num3 > 19) {
     return (num3 - 19) * 3;
   } else {
-    return num3 - 19;
+    return 19 - num3;
   }
 }
-console.log(crazyDiff(20));
-console.log(crazyDiff(18));
+
+const diff = crazyDiff(12)
+console.log(diff)
+
+
 /* ESERCIZIO 4
  Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
  se n è uguale a 400.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-function boundary(n) {
-  return (n > 20 && n <= 100) || n === 400;
+const boundary = function (n) {
+  if (n > 20 && n <= 100 || n === 400 ) {
+    return true
+  }else{
+    return false ;
+  }
+  
 }
-console.log(boundary(20));
-console.log(boundary(400));
-console.log(boundary(13));
-console.log(boundary(50));
+console.log (boundary(77))
 /* ESERCIZIO 5
  Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
  La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
@@ -55,15 +67,16 @@ console.log(boundary(50));
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-function epify(parameter) {
-  if (parameter.startsWitch("EPICODE")) {
-    return parameter;
+const epify = (string) => {
+  if (string.startsWith("EPICODE")) {    // 
+    return string;
   } else {
-    return "EPICODE" + parameter;
+    return "EPICODE" + " " + string;
   }
 }
-console.log(epify("Sono Dario!"));
-console.log(epify("EPICODE:Sono Dario!"));
+console.log(epify("Ciao sono Dario"))
+console.log(epify("EPICODE,Ciao sono Dario"))
+
 
 /* ESERCIZIO 6
  Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
@@ -71,8 +84,12 @@ console.log(epify("EPICODE:Sono Dario!"));
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-function check3and7(positivenumber) {
-  return positivenumber % 3 === 0 || positivenumber % 7 === 0;
+const check3and7 = (num) => {
+ if (num % 3 === 0 && num >= 0 || num % 7 === 0 && num >= 0) {
+  return true
+ }else{
+    return false
+ }
 }
 console.log(check3and7(7));
 console.log(check3and7(3));
@@ -82,17 +99,34 @@ console.log(check3and7(20));
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-function reverseString(string) {
-  return string.split("").revenge("").joint("");
+const reverseString =(string) => {
+  let splitString = string.split("")
+  let reverseString = splitString.reverse()
+  let finalString = reverseString.join("")
+  return finalString;
 }
-console.log(reverseString("Hello!"));
-console.log(reverseString("EPICODE"));
+console.log(reverseString("EPICODE"))
+
 /* ESERCIZIO 8
  Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
  La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+const upperFirst = (string) => {
+  let words = string.split(" ")
+  let finalString = []
+  for (let i = 0; i < words.length; i++) {
+    let firstChar = words[i].charAt(0);
+    let upperCaseChar = firstChar.toUpperCase();
+    let cutString = words[i].slice(1);
+    let finalWord = upperCaseChar + cutString;
+    finalString.push(finalWord);
+    
+  }
+  console.log(finalString.join(" "))
+}
+upperFirst("ciao mi chiamo dario")
 
 /* ESERCIZIO 9
  Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
